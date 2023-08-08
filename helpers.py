c01c519cb76f84d0c6bdcebbe32d6fab7516175c
Nikita Pinaev <nikitapinaev1@gmail.com>
2023-08-08 23:03:29 +0300

    refactor issues getting logic to json report parsing

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 severity = {
+    0: "Info",
     1: "Info",
     2: "Low",
     3: "Medium",
@@ -7,25 +8,13 @@ severity = {
 }
 
 
-def check_dynamic(stingray, module_id):
-    modules = stingray.get_modules().json()
-
-    for m in modules:
-        if module_id == m['id'] and m['tag'] == 'sast':
-            return "False"
-    return "True"
-
-
 def get_scan_creation_date(stingray, scan_id):
     return stingray.get_scan_info(scan_id).json()['created_at'][0:10]
 
 
-def get_localization(stingray, issue_info):
-    issue_data_keys_resp = stingray.get_localization_issue_data_keys()
-    issue_data_keys = issue_data_keys_resp.json()
-
+def get_localization(issue_data_keys, issue_info):
     result_string = ""
-    for k, v in issue_info['items'][0]['info'].items():
+    for k, v in issue_info.items():
         if k in issue_data_keys:
             result_string += f"{issue_data_keys[k]}: {v}\n"
         else:
--- import_scan.py
+++ import_scan.py
@@ -6,7 +6,7 @@ import requests
 import urllib3
 from mdast_cli_core.api import mDastAPI as mDast
 
-from helpers import check_dynamic, get_localization, get_scan_creation_date, severity
+from helpers import get_localization, get_scan_creation_date, severity
 
 
 def parse_args():
@@ -31,27 +31,20 @@ def parse_args():
     return arguments
 
 
-def get_scan(scan_id):
-    logging.info('Collecting scan results..')
+def get_scan(import_scan_id):
+    logging.info(f'Collecting scan {import_scan_id} results..')
     dast_info = {'findings': []}
-    dast_issues_resp = stingray.get_dast_issues(scan_id)
-    dast_issues = dast_issues_resp.json()['results']
-    logging.info(f'Found {len(dast_issues)} issues')
+    issue_data_keys = stingray.get_localization_issue_data_keys().json()
+    dast_issues = stingray.download_scan_json_result(import_scan_id).json()['defects']
     for issue in dast_issues:
-        get_issue_resp = stingray.get_issue_info(issue['id'])
-        issue_info = get_issue_resp.json()
-        rg = issue_info['project_issue']['type']['guide']
         try:
             issue_data_for_dojo = {
-                "title": f"STG-{issue['id']}: {issue['title']}",
-                "description": f"{issue_info['project_issue']['type']['description']}\n\nScan: "
-                               f"{url.strip('/rest')}/scans/{scan_id}/defects\n\nLocalization: "
-                               f"\n{get_localization(stingray, issue_info)}",
-                "severity": f"{severity.get(issue_info['project_issue']['severity'])}",
-                "mitigation": f"{issue_info['project_issue']['type']['requirement']}",
-                "cve": f"{issue['id']}",
-                "dynamic_finding": check_dynamic(stingray, issue_info['project_issue']['type']['module']),
-                "references": f"{url.strip('/rest')}{rg}"
+                "title": f"{issue['name']}: {issue['description']}",
+                "description": f"{get_localization(issue_data_keys, issue['details'][0])}",
+                "severity": f"{severity[issue['severity']]}",
+                "mitigation": f"{issue['requirement']}",
+                "cve": f"{issue['name']}",
+                "references": f"{issue['recommendations']}"
             }
             dast_info['findings'].append(issue_data_for_dojo)
         except ValueError:
@@ -69,7 +62,7 @@ def import_scan_to_dojo(file_path):
         'scan_type': 'Generic Findings Import',
         'environment': dojo_environment,
         'minimum_severity': dojo_min_severity,
-        'scan_date': get_scan_creation_date(stingray, scan)
+        'scan_date': get_scan_creation_date(stingray, scan_id)
 
     }
     form_data = {
@@ -92,26 +85,26 @@ if __name__ == '__main__':
     url = args.url
     login = args.login
     password = args.password
-    scan = args.scan_id
+    scan_id = args.scan_id
     dojo_url = args.dojo_url
     dojo_api_key = args.dojo_key
     engagement_id = args.engagement_id
     dojo_environment = args.dojo_environment
     dojo_min_severity = args.dojo_min_severity
-    file_name = f'{args.file_name}_{scan}_defectDojo_format.json'
+    file_name = f'{args.file_name}_{scan_id}_defectDojo_format.json'
 
     # get scan from sting
     logging.info('Logging in Stingray..')
     stingray = mDast(url, login, password)
     logging.info(f"Logged in as {stingray.current_context['username']}")
-    scan_info = get_scan(scan)
+    scan_info = get_scan(scan_id)
     logging.info('Saving issues to json in generic DefectDojo format...')
     with open(file_name, 'w') as outfile:
         json.dump(scan_info, outfile, sort_keys=False, indent=4, ensure_ascii=False)
-    logging.info(f'{file_name} with all issues for scan {scan} was successfully created')
+    logging.info(f'{file_name} with issues from scan {scan_id} was successfully created')
 
     # import to dojo
-    logging.info(f'Importing scan issues {scan} from {file_name} to engagement {engagement_id}')
+    logging.info(f'Importing scan issues {scan_id} from {file_name} to engagement {engagement_id}')
     import_dojo_resp = import_scan_to_dojo(file_name)
     if import_dojo_resp.status_code == 201:
         logging.info('Success!')
